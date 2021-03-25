6813b18b51ba73423b9e65ceb73ed09085a9ee80
JosFernando <67837209+JosFernando@users.noreply.github.com>
2021-03-25 10:10:38 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
-document.querySelector("div[wfd-id='65']").classList = 'col-12 col-lg-12';
+
+document.getElementById("file").style.display = "none";
+document.getElementById("selector").style.display = "block";
+
+
+var loader = function(e) {
+    let file = e.target.files;
+
+    let show = "<span>Ficheiro Selecionado : </span>" + file[0].name;
+
+    let output = document.getElementById("selector");
+    output.innerHTML = show;
+    output.classList.add("active");
+};
+
+let fileInput = document.getElementById("file");
+fileInput.addEventListener("change", loader);
